c9021f5247f36d55e8e78901b5db72cc068199ef
Saulo <sauloerick@hotmail.com>
2021-01-09 23:30:03 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './NavBar';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home />
     </div>
   );
 }
